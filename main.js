dd802cd01fa8f41578a412faffdbebe863df2736
Dawinzon Polo <arleipolo15@gmail.com>
2025-06-20 11:37:21 -0500

    TestModule3Week1

---
+++ main.js
@@ -0,0 +1,30 @@
+const validateAge = () => {
+    let fullName = APIKEY
+    let Age = document.getElementById("age").value
+
+    if (Age < 0) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Enter a correct value",
+        });
+    } else {
+        if (Age < 18) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "You are a minor Keep learning and enjoying the code!",
+            });
+        } else {
+            if (Age > 18) {
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Welcome ${fullName} you are of legal age. Get ready for great opportunities in the world of programming!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        }
+    }
+}
